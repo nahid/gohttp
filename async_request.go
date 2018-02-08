d91a6aacda867657e931f8ae05386378d4d2d889
Mehedi Hasan Nahid <nahid@pathao.com>
2018-02-08 12:51:26 +0600

    added tests for async request

--- async_request.go
+++ async_request.go
@@ -4,37 +4,38 @@ import (
 	"bytes"
 )
 
+
 // AsyncGet is a asynchronous get http request
-func (req *Request) AsyncGet(url string, ch chan<- *ChannelResponse) {
+func (req *Request) AsyncGet(url string, ch chan<- *AsyncResponse) {
 	go req.makeAsyncRequest("get", url, req.formVals, ch)
 }
 
 // AsyncPost is a asynchronous post http request
-func (req *Request) AsyncPost(url string, ch chan<- *ChannelResponse) {
+func (req *Request) AsyncPost(url string, ch chan<- *AsyncResponse) {
 	go req.makeAsyncRequest("post", url, req.formVals, ch)
 }
 
 // AsyncPut is a asynchronous put http request
-func (req *Request) AsyncPut(url string, ch chan<- *ChannelResponse) {
+func (req *Request) AsyncPut(url string, ch chan<- *AsyncResponse) {
 	go req.makeAsyncRequest("put", url, req.formVals, ch)
 }
 
 // AsyncDelete is a asynchronous delete http request
-func (req *Request) AsyncDelete(url string, ch chan<- *ChannelResponse) {
+func (req *Request) AsyncDelete(url string, ch chan<- *AsyncResponse) {
 	go req.makeAsyncRequest("delete", url, req.formVals, ch)
 }
 
 // AsyncPatch is a asynchronous patch http request
-func (req *Request) AsyncPatch(url string, ch chan<- *ChannelResponse) {
+func (req *Request) AsyncPatch(url string, ch chan<- *AsyncResponse) {
 	go req.makeAsyncRequest("patch", url, req.formVals, ch)
 }
 
 // makeAsyncRequest generate asynchronous request
-func (req *Request) makeAsyncRequest(verb, uri string, payloads *bytes.Buffer, ch chan<- *ChannelResponse) {
-	var res *ChannelResponse
+func (req *Request) makeAsyncRequest(verb, uri string, payloads *bytes.Buffer, ch chan<- *AsyncResponse) {
+	var res *AsyncResponse
 	resp, err := req.makeRequest(verb, uri, payloads)
 
-	res = &ChannelResponse{
+	res = &AsyncResponse{
 		Resp: resp,
 		Err: err,
 	}
--- async_requests_test.go
+++ async_requests_test.go
@@ -7,24 +7,142 @@ func TestAsyncGetRequest(t *testing.T) {
 	t.Log("Sending GET async request... (expected http code: 200)")
 
 	req := NewRequest()
-	ch := make(chan *Response)
+	ch := make(chan *AsyncResponse)
 
 	for i := 0; i <= 100; i++ {
 		req.AsyncGet("http://httpbin.org/get", ch)
 	}
 
 	for i := 0; i <= 100; i++ {
-		resp, err := <-ch
+		aRes := <-ch
 
-		if err != true {
-			t.Error(err)
+		if aRes.Err != nil {
+			t.Error(aRes.Err)
 		}
 
-		if resp.GetStatusCode() != 200 {
+		if aRes.Resp.GetStatusCode() != 200 {
 			t.Error(
 				"For", "GET http://httpbin.org/get",
 				"expected", 200,
-				"got", resp.GetStatusCode(),
+				"got", aRes.Resp.GetStatusCode(),
+			)
+		}
+	}
+
+}
+
+func TestAsyncPostRequest(t *testing.T) {
+	t.Log("Sending POST async request... (expected http code: 200)")
+
+	req := NewRequest()
+	ch := make(chan *AsyncResponse)
+
+	for i := 0; i <= 100; i++ {
+		req.AsyncPost("http://httpbin.org/post", ch)
+	}
+
+	for i := 0; i <= 100; i++ {
+		aRes := <-ch
+
+		if aRes.Err != nil {
+			t.Error(aRes.Err)
+		}
+
+		if aRes.Resp.GetStatusCode() != 200 {
+			t.Error(
+				"For", "POST http://httpbin.org/post",
+				"expected", 200,
+				"got", aRes.Resp.GetStatusCode(),
+			)
+		}
+	}
+
+}
+
+
+func TestAsyncPutRequest(t *testing.T) {
+	t.Log("Sending PUT async request... (expected http code: 200)")
+
+	req := NewRequest()
+	ch := make(chan *AsyncResponse)
+
+	for i := 0; i <= 100; i++ {
+		req.AsyncPut("http://httpbin.org/put", ch)
+	}
+
+	for i := 0; i <= 100; i++ {
+		aRes := <-ch
+
+		if aRes.Err != nil {
+			t.Error(aRes.Err)
+		}
+
+		if aRes.Resp.GetStatusCode() != 200 {
+			t.Error(
+				"For", "PUT http://httpbin.org/put",
+				"expected", 200,
+				"got", aRes.Resp.GetStatusCode(),
+			)
+		}
+	}
+
+}
+
+
+
+
+func TestAsyncPatchRequest(t *testing.T) {
+	t.Log("Sending PATCH async request... (expected http code: 200)")
+
+	req := NewRequest()
+	ch := make(chan *AsyncResponse)
+
+	for i := 0; i <= 100; i++ {
+		req.AsyncPatch("http://httpbin.org/patch", ch)
+	}
+
+	for i := 0; i <= 100; i++ {
+		aRes := <-ch
+
+		if aRes.Err != nil {
+			t.Error(aRes.Err)
+		}
+
+		if aRes.Resp.GetStatusCode() != 200 {
+			t.Error(
+				"For", "PATCH http://httpbin.org/patch",
+				"expected", 200,
+				"got", aRes.Resp.GetStatusCode(),
+			)
+		}
+	}
+
+}
+
+
+
+func TestAsyncDeleteRequest(t *testing.T) {
+	t.Log("Sending DELETE async request... (expected http code: 200)")
+
+	req := NewRequest()
+	ch := make(chan *AsyncResponse)
+
+	for i := 0; i <= 100; i++ {
+		req.AsyncDelete("http://httpbin.org/delete", ch)
+	}
+
+	for i := 0; i <= 100; i++ {
+		aRes := <-ch
+
+		if aRes.Err != nil {
+			t.Error(aRes.Err)
+		}
+
+		if aRes.Resp.GetStatusCode() != 200 {
+			t.Error(
+				"For", "DELETE http://httpbin.org/delete",
+				"expected", 200,
+				"got", aRes.Resp.GetStatusCode(),
 			)
 		}
 	}
