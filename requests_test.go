5313d4251ba9e42cd503b689aa2c4a375927fcb1
Nahid Bin Azhar <nahid.dns@gmail.com>
2018-02-08 13:40:46 +0600

    added some tests for request

--- requests_test.go
+++ requests_test.go
@@ -8,7 +8,7 @@ func TestGetRequest(t *testing.T) {
 
 	req := NewRequest()
 
-	resp, err := req.Get("http://httpbin.org/get")
+	resp, err := req.Query(map[string]string{"q": "hello"}).Get("http://httpbin.org/get")
 
 	if err != nil {
 		t.Error(err)
@@ -29,16 +29,104 @@ func TestPostRequest(t *testing.T) {
 
 	req := NewRequest()
 
-	resp, err := req.Post("http://httpbin.org/post")
+	resp, err := req.FormData(map[string]string {
+		"name": "Nahid",
+	}).Post("http://httpbin.org/post")
 
 	if err != nil {
 		t.Error(err)
 	}
 
+
 	if resp.GetStatusCode() != 200 {
+
 		t.Error(
 			"For", "POST http://httpbin.org/post",
-			"expected", 200,
+			"expected", "200",
+			"got", resp.GetStatusCode(),
+		)
+	}
+}
+
+
+// TestPutRequest tests PUT request
+func TestPutRequest(t *testing.T) {
+	t.Log("Sending PUT request... (expected http code: 200)")
+
+	req := NewRequest()
+
+	resp, err := req.FormData(map[string]string {
+		"name": "Nahid",
+	}).JSON(map[string]interface{}{
+		"website": "www.nahid.im",
+	}).Put("http://httpbin.org/put")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+
+	if resp.GetStatusCode() != 200 {
+
+		t.Error(
+			"For", "PUT http://httpbin.org/put",
+			"expected", "200",
+			"got", resp.GetStatusCode(),
+		)
+	}
+}
+
+// TestPatchRequest tests POST request
+func TestPatchRequest(t *testing.T) {
+	t.Log("Sending PATCH request... (expected http code: 200)")
+
+	req := NewRequest()
+
+	resp, err := req.FormData(map[string]string {
+		"name": "Nahid",
+	}).Headers(map[string]string{
+		"Custom-Header": "nothing",
+	}).Patch("http://httpbin.org/patch")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+
+	if resp.GetStatusCode() != 200 {
+
+		t.Error(
+			"For", "PATCH http://httpbin.org/patch",
+			"expected", "200",
+			"got", resp.GetStatusCode(),
+		)
+	}
+}
+
+
+
+// TestPatchRequest tests DELETE request
+func TestDeleteRequest(t *testing.T) {
+	t.Log("Sending DELETE request... (expected http code: 200)")
+
+	req := NewRequest()
+
+	resp, err := req.Body([]byte{
+
+	}).Headers(map[string]string{
+		"Custom-Header": "nothing",
+	}).Delete("http://httpbin.org/delete")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+
+	if resp.GetStatusCode() != 200 {
+
+		t.Error(
+			"For", "DELETE http://httpbin.org/delete",
+			"expected", "200",
 			"got", resp.GetStatusCode(),
 		)
 	}
